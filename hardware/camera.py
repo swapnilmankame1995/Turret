d3ebd0d737f61159ef3e14d4c50b62789531bf25
Swapnil mankame <swapnilum95@gmail.com>
2026-03-24 18:14:26 +0530

    camera issue

--- hardware/camera.py
+++ hardware/camera.py
@@ -3,9 +3,12 @@ import cv2
 
 class Camera:
     def __init__(self, index=0, width=640, height=480):
-        self.cap = cv2.VideoCapture(index)
+        # Force DirectShow backend (FIXES FREEZE)
+        self.cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
+
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
     def get_frame(self):
         ret, frame = self.cap.read()
